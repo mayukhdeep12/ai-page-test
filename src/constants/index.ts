b3d828e002bfa04011676b512d98ec0c77b29f9d
mayukhdeep12 <mayukhdeep12mathur@yahoo.com>
2024-12-19 19:00:50 +0530

    resp4

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -522,37 +522,3 @@ export const hightlightsSlidesProject2 = [
         },
 ];
 
-export const hightlightsSlides = [
-	{
-		id: 1,
-		textLists: [
-			"Enter A17 Pro.",
-			"Game‑changing chip.",
-			"Groundbreaking performance.",
-		],
-		video: highlightFirstmv,
-		videoDuration: 4,
-	},
-	{
-		id: 2,
-		textLists: ["Titanium.", "So strong. So light. So Pro."],
-		video: highlightSectmv,
-		videoDuration: 5,
-	},
-	{
-		id: 3,
-		textLists: [
-			"iPhone 15 Pro Max has the",
-			"longest optical zoom in",
-			"iPhone ever. Far out.",
-		],
-		video: highlightThirdmv,
-		videoDuration: 2,
-	},
-	{
-		id: 4,
-		textLists: ["All-new Action button.", "What will yours do?."],
-		video: highlightFourthmv,
-		videoDuration: 3.63,
-	},
-];
